9ef2514aabd2456ab0be06497cf01c2742059cf3
Valentine Schalckens <v.schalckens@gmail.com>
2021-10-12 15:30:01 +0200

    Avancement test intrusion

---
+++ tests/mail/testSecuCode.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * "C:\wamp64\bin\php\php7.3.21\php.exe" testSecuCode.php
+ */
+
+$codeVisiteur = rand(1000,9999);
+
+$cpt = 0;
+$tmp = microtime(true);
+for ($i = 1000; $i < 10000; $i++) {
+    if ($codeVisiteur == $i) {
+        $tmp = microtime(true) - $tmp ;
+        echo "\n";
+        echo "Success after " . $cpt . " try ";
+        echo "\n";
+        echo "fin :" . $tmp;
+        echo "\n";
+    } else {
+        $cpt++;
+    }
+}
+
