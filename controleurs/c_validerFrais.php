8382f607768680d83239153ece10ee79c3e26911
Valentine Schalckens <v.schalckens@gmail.com>
2021-10-19 14:38:23 +0200

    changement de zone de projet

--- controleurs/c_validerFrais.php
+++ controleurs/c_validerFrais.php
@@ -6,3 +6,31 @@
  * and open the template in the editor.
  */
 
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+$idVisiteur = $_SESSION['idVisiteur'];
+switch ($action) {
+case 'selectionnerMois':
+    $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+    // Afin de sélectionner par défaut le dernier mois dans la zone de liste
+    // on demande toutes les clés, et on prend la première,
+    // les mois étant triés décroissants
+    $lesCles = array_keys($lesMois);
+    $moisASelectionner = $lesCles[0];
+    include 'vues/v_listeMois.php';
+    break;
+case 'voirEtatFrais':
+    $leMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
+    $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+    $moisASelectionner = $leMois;
+    include 'vues/v_listeMois.php';
+    $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $leMois);
+    $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $leMois);
+    $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $leMois);
+    $numAnnee = substr($leMois, 0, 4);
+    $numMois = substr($leMois, 4, 2);
+    $libEtat = $lesInfosFicheFrais['libEtat'];
+    $montantValide = $lesInfosFicheFrais['montantValide'];
+    $nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
+    $dateModif = dateAnglaisVersFrancais($lesInfosFicheFrais['dateModif']);
+    include 'vues/v_etatFrais.php';
+}
